1b642c7e76a8ac837438a3c7057b1cf5ceba9f80
Connor Henry <cjhenry10@gmail.com>
2024-02-22 14:06:30 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bulk-zoom-invites
+
+This extension will help you respond to multiple Zoom meeting invites at one time.
+
+_Important note: The invites need to be consecutive in your inbox. If you received an email that separates the bulk invites, you will need to run the extension again beginning with the Zoom invite closest to the top of your inbox._
+
+1. Open the Zoom meeting invite email closest to the top of your inbox.
+2. Set your attendance (Yes, No, Maybe) for every meeting using the dropdown menu.
+3. Click the Start button to set your attendance.
---
+++ index.js
@@ -0,0 +1,49 @@
+async function test() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const views = chrome.extension.getViews({ type: 'popup' });
+  let attendance = 'Yes';
+  if (views.length > 0) {
+    attendance = views[0].document.getElementById('attendance-status').value;
+  }
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    args: [attendance],
+    function: (params) => {
+      // start by clicking the button matching the attendance status: Yes, Maybe, or No
+      // this selector narrows down the buttons to ~12, then it finds the text content that matches the attendance status
+      Array.from(document.querySelectorAll("td div div[role='button']"))
+        .find((el) => el.textContent === params)
+        .click();
+
+      // mutation observer to watch for DOM changes
+      var observer = new MutationObserver(() => {
+        // clear the timeout if it exists
+        if (window.timerId) {
+          clearTimeout(window.timerId);
+        }
+        // set a new timeout to wait 150ms for mutations to stop
+        window.timerId = setTimeout(function () {
+          // find the button that matches the attendance status
+          let attendanceButton = Array.from(
+            document.querySelectorAll("td div div[role='button']")
+          ).find((el) => el.textContent === params);
+
+          if (attendanceButton) {
+            // if the button exists, click it and go to the next email
+            document.querySelectorAll("[aria-label='Older']")[1].click();
+            attendanceButton.click();
+            // attendanceButton = null;
+          } else {
+            // if the button doesn't exist, stop the observer
+            observer.disconnect();
+          }
+        }, 150);
+      });
+      observer.observe(document, { childList: true, subtree: true });
+    },
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('test').addEventListener('click', test);
+});
